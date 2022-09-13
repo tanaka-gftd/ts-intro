ce6d759484ab8c5faccbeee93384bf5ba42c265f
Tanaka <f.tanaka@gftd.works>
2022-09-13 14:04:43 +0900

    TypeScriptの型の学習

--- index.js
+++ index.js
@@ -7,3 +7,19 @@ const cat = {
 };
 //猫の色をコンソールに出力
 console.log('猫は ' + cat.color + ' 色です');
+/* 型の設定についての学習 */
+//Arrayの型の設定方法は２種類存在
+const scores = [10, 20, 30];
+const scores2 = [10, 20, 30];
+function calcAverage(values) {
+    let sum = 0;
+    for (const value of values) {
+        sum += value;
+    }
+    const avg = sum / values.length;
+    return avg;
+}
+const averageScore = calcAverage(scores);
+const averageScore2 = calcAverage(scores2);
+console.log(averageScore);
+console.log(averageScore2);
--- index.ts
+++ index.ts
@@ -7,3 +7,25 @@ const cat = {
 
 //猫の色をコンソールに出力
 console.log('猫は ' + cat.color + ' 色です');
+
+
+/* 型の設定についての学習 */
+
+//Arrayの型の設定方法は２種類存在
+const scores: number[] = [10, 20, 30];
+const scores2: Array<number> = [10, 20, 30];
+
+function calcAverage(values: number[]): number {
+  let sum: number = 0;
+  for(const value of values) {
+    sum += value;  
+  }
+  const avg: number = sum/values.length;
+  return avg;
+}
+
+const averageScore: number = calcAverage(scores);
+const averageScore2: number = calcAverage(scores2);
+
+console.log(averageScore);
+console.log(averageScore2);
