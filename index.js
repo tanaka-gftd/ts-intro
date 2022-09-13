2da2f00d2e662fa1e0325da627d06022969748df
Tanaka <f.tanaka@gftd.works>
2022-09-13 11:41:52 +0900

    TypeScriptの学習

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
+"use strict";
 //猫のオブジェクトを作成
 const cat = {
-  name: '太郎',
-  height: 25,
-  color: '白'
+    name: '太郎',
+    height: 25,
+    color: '白'
 };
-
 //猫の色をコンソールに出力
 console.log('猫は ' + cat.color + ' 色です');
